40fdd5732abbc62ea10e68dbed149bf191d799fd
NWojt <n.wojtowicz098@gmail.com>
2020-12-16 00:34:23 +0100

    ER Zagadnienie Posrednika # Added entities classes # Added controler class # Initial maven commit

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,7 +3,7 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"application", "control", "entities"})
 public class Application {
     public static void main(String[] args){
 
