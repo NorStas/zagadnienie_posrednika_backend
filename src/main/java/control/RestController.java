5fa8b09b5b04d55da807190f788855ba07c32bbb
NWojt <n.wojtowicz098@gmail.com>
2020-12-16 00:30:19 +0100

    ER Zagadnienie Posrednika # Added entities classes # Added controler class # Initial maven commit

--- src/main/java/control/RestController.java
+++ src/main/java/control/RestController.java
@@ -1,4 +1,36 @@
 package control;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.JsonEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@org.springframework.web.bind.annotation.RestController
+@CrossOrigin
 public class RestController {
+
+    private static Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+
+    @RequestMapping(value="/middleman", method = RequestMethod.POST)
+    public ResponseEntity<String> calculateMiddleman(@Valid @RequestBody String data) {
+        JsonEntity entity = gson.fromJson(data, JsonEntity.class);
+
+        JsonEntity response = new JsonEntity();
+        return ResponseEntity.ok(gson.toJson(response));
+    }
+
+    @RequestMapping(value="/linear", method = RequestMethod.POST)
+    public ResponseEntity<String> calculateLinear(@Valid @RequestBody String data) {
+        JsonEntity entity = gson.fromJson(data, JsonEntity.class);
+        JsonEntity response = new JsonEntity();
+
+        return ResponseEntity.ok(gson.toJson(response));
+    }
 }
---
+++ src/main/java/entities/JsonEntity.java
@@ -0,0 +1,187 @@
+package entities;
+
+public class JsonEntity {
+    private double supplier1;
+    private double supplier2;
+    private double recipient1;
+    private double recipient2;
+    private double supplier3;
+    private double supplier4;
+    private double supplier1fee;
+    private double supplier2fee;
+    private double recipient1fee;
+    private double recipient2fee;
+    private double supplier3fee;
+    private double supplier4fee;
+    private double sup1rec1cost;
+    private double sup1rec2cost;
+    private double sup2rec1cost;
+    private double sup2rec2cost;
+    private double sup3rec1cost;
+    private double sup3rec2cost;
+    private double sup4rec1cost;
+    private double sup4rec2cost;
+
+    public JsonEntity() {
+    }
+
+    public double getSupplier1() {
+        return supplier1;
+    }
+
+    public void setSupplier1(double supplier1) {
+        this.supplier1 = supplier1;
+    }
+
+    public double getSupplier2() {
+        return supplier2;
+    }
+
+    public void setSupplier2(double supplier2) {
+        this.supplier2 = supplier2;
+    }
+
+    public double getRecipient1() {
+        return recipient1;
+    }
+
+    public void setRecipient1(double recipient1) {
+        this.recipient1 = recipient1;
+    }
+
+    public double getRecipient2() {
+        return recipient2;
+    }
+
+    public void setRecipient2(double recipient2) {
+        this.recipient2 = recipient2;
+    }
+
+    public double getSupplier3() {
+        return supplier3;
+    }
+
+    public void setSupplier3(double supplier3) {
+        this.supplier3 = supplier3;
+    }
+
+    public double getSupplier4() {
+        return supplier4;
+    }
+
+    public void setSupplier4(double supplier4) {
+        this.supplier4 = supplier4;
+    }
+
+    public double getSupplier1fee() {
+        return supplier1fee;
+    }
+
+    public void setSupplier1fee(double supplier1fee) {
+        this.supplier1fee = supplier1fee;
+    }
+
+    public double getSupplier2fee() {
+        return supplier2fee;
+    }
+
+    public void setSupplier2fee(double supplier2fee) {
+        this.supplier2fee = supplier2fee;
+    }
+
+    public double getRecipient1fee() {
+        return recipient1fee;
+    }
+
+    public void setRecipient1fee(double recipient1fee) {
+        this.recipient1fee = recipient1fee;
+    }
+
+    public double getRecipient2fee() {
+        return recipient2fee;
+    }
+
+    public void setRecipient2fee(double recipient2fee) {
+        this.recipient2fee = recipient2fee;
+    }
+
+    public double getSupplier3fee() {
+        return supplier3fee;
+    }
+
+    public void setSupplier3fee(double supplier3fee) {
+        this.supplier3fee = supplier3fee;
+    }
+
+    public double getSupplier4fee() {
+        return supplier4fee;
+    }
+
+    public void setSupplier4fee(double supplier4fee) {
+        this.supplier4fee = supplier4fee;
+    }
+
+    public double getSup1rec1cost() {
+        return sup1rec1cost;
+    }
+
+    public void setSup1rec1cost(double sup1rec1cost) {
+        this.sup1rec1cost = sup1rec1cost;
+    }
+
+    public double getSup1rec2cost() {
+        return sup1rec2cost;
+    }
+
+    public void setSup1rec2cost(double sup1rec2cost) {
+        this.sup1rec2cost = sup1rec2cost;
+    }
+
+    public double getSup2rec1cost() {
+        return sup2rec1cost;
+    }
+
+    public void setSup2rec1cost(double sup2rec1cost) {
+        this.sup2rec1cost = sup2rec1cost;
+    }
+
+    public double getSup2rec2cost() {
+        return sup2rec2cost;
+    }
+
+    public void setSup2rec2cost(double sup2rec2cost) {
+        this.sup2rec2cost = sup2rec2cost;
+    }
+
+    public double getSup3rec1cost() {
+        return sup3rec1cost;
+    }
+
+    public void setSup3rec1cost(double sup3rec1cost) {
+        this.sup3rec1cost = sup3rec1cost;
+    }
+
+    public double getSup3rec2cost() {
+        return sup3rec2cost;
+    }
+
+    public void setSup3rec2cost(double sup3rec2cost) {
+        this.sup3rec2cost = sup3rec2cost;
+    }
+
+    public double getSup4rec1cost() {
+        return sup4rec1cost;
+    }
+
+    public void setSup4rec1cost(double sup4rec1cost) {
+        this.sup4rec1cost = sup4rec1cost;
+    }
+
+    public double getSup4rec2cost() {
+        return sup4rec2cost;
+    }
+
+    public void setSup4rec2cost(double sup4rec2cost) {
+        this.sup4rec2cost = sup4rec2cost;
+    }
+}
